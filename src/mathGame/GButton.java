db5785509015be1474c4836edc6f800f08c7519a
mlmingo985 <mlmingo985@gmail.com>
2016-10-19 13:48:27 -0400

    restructuring picture location

--- src/mathGame/GButton.java
+++ src/mathGame/GButton.java
@@ -15,7 +15,6 @@ import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 /**
