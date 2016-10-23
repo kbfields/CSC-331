29ca2ca089b404cbe7194cb013d08237d30eda66
mlmingo985 <mlmingo985@gmail.com>
2016-10-23 15:13:38 -0400

    gbutton action listener working

--- src/mathGame/GButton.java
+++ src/mathGame/GButton.java
@@ -23,11 +23,10 @@ import javax.swing.SwingUtilities;
  */
 public class GButton extends JButton implements ActionListener  {
 	private Image img;
-	private boolean answered;
-	private boolean working;
 	private int w;
 	private int h;
 	byte value = 0;
+	private int rotate = 0;
 	/**
 	 * 
 	 */
@@ -38,8 +37,7 @@ public class GButton extends JButton implements ActionListener  {
 		w = img.getWidth(this);
 		h = img.getHeight(this);
 		setPreferredSize(new Dimension(w, h));
-		answered = false;
-		working = false;
+		this.addActionListener(this);
 	}
 	
 	public void actionPerformed(ActionEvent e){
@@ -47,48 +45,37 @@ public class GButton extends JButton implements ActionListener  {
 		value%=3;
 		switch(value){
 			case 0:
-				
+				rotate = 0;
 				break;
 			case 1:
-				
+				rotate = 1;
 				break;
 			case 2:
-				
+				rotate = 2;
 				break;
 		   }
 		}
 
-	public void setAnswered() {
-		answered = true;
-	}
 
-	public boolean getAnswered() {
-		return answered;
-	}
 
 	public void paintComponent(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
-		if (!answered) {
-			if (working){
-				g2.setColor(Color.lightGray);
-				g2.fillRect(0, 0, w, h);
-				g2.setPaint(Color.yellow);
-				g2.setFont(new Font("Sans-serif", Font.BOLD, 20));
-				g2.drawString("Testing, one, two, three.", w/3, h/2);
-				((JFrame)SwingUtilities.getRoot(this)).setTitle("Testing, one, two, three.");
-			}
-			else{
-				g2.setColor(Color.darkGray);
-				g2.fillRect(0, 0, w, h);
-			}
-			working = !working;  // toggles on and off
+		if (rotate == 0){
+			g2.setColor(Color.darkGray);
+			g2.fillRect(0, 0, w, h);
+		} else if (rotate == 1) {
+			g2.setColor(Color.lightGray);
+			g2.fillRect(0, 0, w, h);
+			g2.setPaint(Color.yellow);
+			g2.setFont(new Font("Sans-serif", Font.BOLD, 20));
+			g2.drawString("Testing, one, two, three.", w/3, h/2);
+			((JFrame)SwingUtilities.getRoot(this)).setTitle("Testing, one, two, three.");
 		} else {
 			g2.drawImage(img, 0, 0, this);
 			((JFrame)SwingUtilities.getRoot(this)).setTitle("Testing GPanel");
 		}
-		answered = !answered; // toggles on and off 
 	}
-
+	
 	public static void main(String[] args) {
 		JFrame f = new JFrame("Testing GPanel - resize, maximize and back to see behavior");
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
