e0799b2dd56ceb8fd45a85397c0da848bfa23bfc
skillapop <skillapop@172.21.132.100>
2016-10-23 23:05:49 -0400

    haaha

--- src/mathGame/GButton.java
+++ src/mathGame/GButton.java
@@ -54,6 +54,7 @@ public class GButton extends JButton implements ActionListener  {
 	public void actionPerformed(ActionEvent e){
 		value++;
 		value%=3;
+		double ans = eval(math);
 		switch(value){
 			case 0:
 				rotate = 0;
@@ -63,8 +64,21 @@ public class GButton extends JButton implements ActionListener  {
 				break;
 			case 2:
 				String guess = JOptionPane.showInputDialog("Type your answer to the given equation.");
-				rotate = 2;
 				System.out.println(Integer.parseInt(guess));
+				if (guess.equals(ans)){
+					rotate = 2;
+					break;
+				}
+				else{
+				String guess2 = JOptionPane.showInputDialog("Type your answer to the given equation.");
+				System.out.println(Integer.parseInt(guess2));
+				if(guess2.equals(ans)){
+					rotate = 2;
+				}
+				else{
+					rotate = 2; 
+				}
+				}
 				System.out.println(math);
 				break;
 		   }
@@ -90,6 +104,72 @@ public class GButton extends JButton implements ActionListener  {
 			g2.drawImage(img, 0, 0, this);
 			((JFrame)SwingUtilities.getRoot(this)).setTitle("Testing GPanel");
 		}
+	}
+	public static double eval(final String str) {
+	    return new Object() {
+	        int pos = -1, ch;
+
+	        void nextChar() {
+	            ch = (++pos < str.length()) ? str.charAt(pos) : -1;
+	        }
+
+	        boolean eat(int charToEat) {
+	            while (ch == ' ') nextChar();
+	            if (ch == charToEat) {
+	                nextChar();
+	                return true;
+	            }
+	            return false;
+	        }
+
+	        double parse() {
+	            nextChar();
+	            double x = parseExpression();
+	            if (pos < str.length()) throw new RuntimeException("Unexpected: " + (char)ch);
+	            return x;
+	        }
+
+	        double parseExpression() {
+	            double x = parseTerm();
+	            for (;;) {
+	                if      (eat('+')) x += parseTerm(); // addition
+	                else if (eat('-')) x -= parseTerm(); // subtraction
+	                else return x;
+	            }
+	        }
+	        double parseTerm() {
+	            double x = parseFactor();
+	            for (;;) {
+	                if      (eat('*')) x *= parseFactor(); // multiplication
+	                else if (eat('/')) x /= parseFactor(); // division
+	                else return x;
+	            }
+	        }
+	        double parseFactor() {
+	            if (eat('+')) return parseFactor(); // unary plus
+	            if (eat('-')) return -parseFactor(); // unary minus
+
+	            double x;
+	            int startPos = this.pos;
+	            if (eat('(')) { // parentheses
+	                x = parseExpression();
+	                eat(')');
+	            } else if ((ch >= '0' && ch <= '9') || ch == '.') { // numbers
+	                while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
+	                x = Double.parseDouble(str.substring(startPos, this.pos));
+	            } else if (ch >= 'a' && ch <= 'z') { // functions
+	                while (ch >= 'a' && ch <= 'z') nextChar();
+	                String func = str.substring(startPos, this.pos);
+	                x = parseFactor();
+	            } else {
+	                throw new RuntimeException("Unexpected: " + (char)ch);
+	            }
+
+	            if (eat('^')) x = Math.pow(x, parseFactor()); // exponentiation
+
+	            return x;
+	        }
+	    }.parse();
 	}
 	
 
